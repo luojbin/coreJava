da298b5b9d8042173295e061abe4b215cf6b1039
luojbin <z1_2x@msn.com>
2018-11-25 22:48:11 +0800

    整理

--- src/main/java/com/loyofo/core/s8_generic/e2_genericClass/f4_limit/L5_InstanceDemo.java
+++ src/main/java/com/loyofo/core/s8_generic/e2_genericClass/f4_limit/L5_InstanceDemo.java
@@ -2,6 +2,8 @@ package com.loyofo.core.s8_generic.e2_genericClass.f4_limit;
 
 import org.junit.Test;
 
+import java.util.function.Supplier;
+
 public class L5_InstanceDemo {
     @Test
     public void test1() {
@@ -16,3 +18,39 @@ public class L5_InstanceDemo {
     }
 }
 
+class Container<T> {
+    T content;
+
+    public Container() {
+        // 不允许直接创建类型参数的实例, 因为类型擦除后会是 new Object(), 显然不是想要的结果
+        // content = new T();
+    }
+
+    public Container(T t) {
+        // 不允许直接创建类型参数的实例, 因为类型擦除后会是 new Object(), 显然不是想要的结果
+        // content = new T();
+        this.content = t;
+    }
+
+    public static <T> Container<T> getInstance(Supplier<T> constructor) {
+        return new Container<>(constructor.get());
+    }
+
+    public static <R> Container<R> reflexInstance(Class<R> cl) {
+        try {
+            return new Container<>(cl.newInstance());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
+
+class Content {
+    private String name = "this is content";
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
