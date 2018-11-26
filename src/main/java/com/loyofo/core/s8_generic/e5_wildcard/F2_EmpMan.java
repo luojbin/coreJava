03a89770d3da5d3ceb7e4d0acb4c5668ee3fee89
luojbin <z1_2x@msn.com>
2018-11-26 18:52:11 +0800

    通配符问题

--- src/main/java/com/loyofo/core/s8_generic/e5_wildcard/F2_EmpMan.java
+++ src/main/java/com/loyofo/core/s8_generic/e5_wildcard/F2_EmpMan.java
@@ -53,4 +53,30 @@ public class F2_EmpMan {
         // 尝试往里面添加别的 animal 类型对象, 编译失败
         // animals.add(new Animal());
     }
+
+    @Test
+    public void test4() {
+        // ? extends Animal, 表示列表内可能是 Animal 的任意子类
+        ArrayList<? extends Animal> animals = new ArrayList<>();
+
+        // 由于不确定里面的元素是什么类型, 因此不接受任何类型的实参, 不能往里面加东西
+        // animals.add(new Dog());
+        // animals.add(new Cat());
+
+        // 由于里面的东西肯定是Animal 的子类, 返回 Animal 不会有错, 所有返回类型都是 Animal
+        Animal animal = animals.get(0);
+    }
+
+    @Test
+    public void test5() {
+        // ? super Animal, 表示列表内可能是 Animal 的任意超类
+        ArrayList<? super Animal> animals = new ArrayList<>();
+
+        // 由于list内能装 Animal 的任意超类, 那么存入 Animal 的子类对象肯定没问题
+        animals.add(new Dog());
+        animals.add(new Cat());
+
+        // 由于里面的东西肯定是Animal 的超类, 无法确定返回的是什么类型, 只能返回 Object,
+        Object animal = animals.get(0);
+    }
 }
