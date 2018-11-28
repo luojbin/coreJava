600619c66ff4e14627c1f44ee7f7f78bda2f5bde
luojbin <z1_2x@msn.com>
2018-11-28 17:27:40 +0800

    反射

--- src/main/java/com/loyofo/core/s5_reflex/e1_Class/F2_Detail.java
+++ src/main/java/com/loyofo/core/s5_reflex/e1_Class/F2_Detail.java
@@ -1,10 +1,10 @@
 package com.loyofo.core.s5_reflex.e1_Class;
 
 import com.loyofo.core.s6_interface.e0_demo.Manager;
+
 import org.junit.Test;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
+import java.lang.reflect.*;
 
 public class F2_Detail {
 
@@ -49,6 +49,81 @@ public class F2_Detail {
             System.out.println(obj);
         }
         System.out.println();
+    }
+
+    @Test
+    public void testParseClass() throws Exception{
+        String name = "com.loyofo.core.s6_interface.e0_demo.Employee";
+        Class cl = Class.forName(name);
+        Class superCl = cl.getSuperclass();
+        String modifiers = Modifier.toString(cl.getModifiers());
+
+        // 拼凑类的声明
+        if (modifiers.length() > 0) System.out.print(modifiers + " ");
+        System.out.print("class " + cl.getSimpleName());
+        if (superCl != null && superCl != Object.class) System.out.println(" extends " + superCl.getSimpleName());
+        System.out.println(" {");
+        // 输出静态域
+        prinFields(cl);
+        System.out.println();
+        // 输出实例域
+        // 输出构造函数
+        printConstruceors(cl);
+        System.out.println();
+        // 输出普通方法
+        printMethods(cl);
+        System.out.println("}");
+    }
+
+    public void printConstruceors(Class cl) {
+        Constructor[] cs = cl.getDeclaredConstructors();
+        for (Constructor c : cs) {
+            String name = c.getName();
+            System.out.print("\t");
+            System.out.print(Modifier.toString(c.getModifiers()) + " ");
+            System.out.print(name + "(");
+            // 输出参数列表
+            Parameter[] parameters = c.getParameters();
+            for (int i = 0; i < parameters.length; i++) {
+                if (i > 0) {
+                    System.out.print(", ");
+                }
+                Parameter p = parameters[i];
+                System.out.print(p.getType().getSimpleName() + " ");
+                System.out.print(p.getName());
+            }
+            System.out.println(")");
+        }
+    }
+    public void printMethods(Class cl) {
+        Method[] ms = cl.getDeclaredMethods();
+        for (Method m : ms) {
+            String name = m.getName();
+            System.out.print("\t");
+            System.out.print(Modifier.toString(m.getModifiers()) + " ");
+            System.out.print(name + "(");
+            // 输出参数列表
+            Parameter[] parameters = m.getParameters();
+            for (int i = 0; i < parameters.length; i++) {
+                if (i > 0) {
+                    System.out.print(", ");
+                }
+                Parameter p = parameters[i];
+                System.out.print(p.getType().getSimpleName() + " ");
+                System.out.print(p.getName());
+            }
+            System.out.println(")");
+        }
+    }
+    public void prinFields(Class cl) {
+        Field[] fields = cl.getDeclaredFields();
+        for (Field f : fields) {
+            String name = f.getName();
+            System.out.print("\t");
+            System.out.print(Modifier.toString(f.getModifiers()) + " ");
+            System.out.print(f.getType().getSimpleName() + " ");
+            System.out.println(name);
+        }
     }
 }
 
