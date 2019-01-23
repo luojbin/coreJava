73f9384aeedd0571a195e881d84ed48f599b52b9
luojbin <z1_2x@msn.com>
2019-01-23 10:31:31 +0800

    panel加点击事件

--- src/main/java/com/loyofo/core/s12_swingLayout/e1_BorderLayout/BorderLayoutFrame.java
+++ src/main/java/com/loyofo/core/s12_swingLayout/e1_BorderLayout/BorderLayoutFrame.java
@@ -2,6 +2,8 @@ package com.loyofo.core.s12_swingLayout.e1_BorderLayout;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 /**
  * @author luojbin
@@ -11,17 +13,24 @@ import java.awt.*;
 public class BorderLayoutFrame extends JFrame {
 
     public BorderLayoutFrame() throws HeadlessException {
-        JPanel panel1 = new InfoPanel("顶部", 200, 50);
-        JPanel panel2 = new InfoPanel("左侧栏", 50, 100);
-        JPanel panel3 = new InfoPanel("中部", 100, 100);
-        JPanel panel4 = new InfoPanel("右侧栏", 50, 100);
-        JPanel panel5 = new InfoPanel("底部", 200, 50);
+        InfoPanel panel1 = new InfoPanel("顶部", 200, 50);
+        InfoPanel panel2 = new InfoPanel("左侧栏", 50, 100);
+        InfoPanel panel3 = new InfoPanel("中部", 100, 100);
+        InfoPanel panel4 = new InfoPanel("右侧栏", 50, 100);
+        InfoPanel panel5 = new InfoPanel("底部", 200, 50);
 
         panel1.setBackground(Color.PINK);
         panel2.setBackground(Color.GRAY);
         panel3.setBackground(Color.GREEN);
         panel4.setBackground(Color.MAGENTA);
         panel5.setBackground(Color.YELLOW);
+
+        panel1.addMouseListener(new PanelMouseListener(panel1));
+        panel2.addMouseListener(new PanelMouseListener(panel2));
+        panel3.addMouseListener(new PanelMouseListener(panel3));
+        panel4.addMouseListener(new PanelMouseListener(panel4));
+        panel5.addMouseListener(new PanelMouseListener(panel5));
+
 
         // BorderLayout 是 JFrame 默认的布局管理器, 分成上下左右中五块, 会将添加到 jFrame 的元素拉伸填满所在区域
         // 如果不指定区域, 则放置在中部
@@ -43,10 +52,27 @@ public class BorderLayoutFrame extends JFrame {
             setPreferredSize(new Dimension(width, height));
         }
 
+        public String getMsg() {
+            return msg;
+        }
+
         @Override
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
             g.drawString(msg, 1*getWidth()/5, getHeight()/2);
+        }
+    }
+
+    class PanelMouseListener extends MouseAdapter {
+        String msg;
+        PanelMouseListener(InfoPanel panel) {
+            msg = panel.getMsg();
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            super.mouseClicked(e);
+            System.out.println(msg);
         }
     }
 }
