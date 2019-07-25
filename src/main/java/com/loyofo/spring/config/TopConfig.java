4580379a03c4ff0ca2dd46ea3c6af0a621ad93a4
luojbin <z1_2x@msn.com>
2019-07-25 10:30:27 +0800

    去掉 spring 部分

--- src/main/java/com/loyofo/spring/config/TopConfig.java
+++
@@ -1,9 +0,0 @@
-package com.loyofo.spring.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-
-// @Configuration
-@Import({ConfigA.class, ConfigB.class})
-public class TopConfig {
-}
